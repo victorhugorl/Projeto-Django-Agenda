08c92822653fe999acce8cc97e11de7f5e1ecfb8
Victor Hugo Rangel <victorlimasg@outlook.com>
2023-12-30 15:38:53 -0300

    atualizando models.py

--- contact/models.py
+++ contact/models.py
@@ -17,6 +17,7 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
