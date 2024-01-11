4f84403c4900f614506c74936e3069857d63e01f
victor <victorlimasg@email.com>
2024-01-11 10:36:19 -0300

    criando uma funcionalidade de busca para a agenda

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
+
 
 # Create your views here.
 
@@ -11,6 +13,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos'
     }
 
     return render(
@@ -18,3 +21,53 @@ def index(request):
         'contact/index.html',
         context,
     )
+
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(
+        Contact.objects,
+        pk=contact_id,
+        show=True,
+    )
+
+    contact_name = f'{single_contact.first_name} {single_contact.last_name}'
+
+    context = {
+        'contact': single_contact,
+        'site_title': contact_name,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context,
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').trip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value) 
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos'
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
